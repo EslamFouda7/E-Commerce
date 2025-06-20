9540461ebf20f898cf362c927e5f0ffc0a721b2f
EslamFouda7 <eslamfouda311@gmail.com>
2025-06-20 19:10:40 +0300

    edit2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { register } from 'swiper/element';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { routes } from './app/app.routes';
 
 register();
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig, // ✅ تمرير إعدادات appConfig
+  providers: [
+    ...appConfig.providers, // ✅ تمرير جميع الـ providers من appConfig
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }) // ✅ التمرير لأعلى عند تغيير الصفحة
+    )
+  ]
+
+}).catch((err) => console.error(err));
